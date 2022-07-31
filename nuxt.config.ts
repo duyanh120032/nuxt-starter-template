3e15ac3a3acd1505230dce11bf7265a8659a87be
*lyZCWKmo9M <duyanh120032@gmail.com>
2022-07-31 21:36:01 +0700

    feat : add windicss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,79 @@
 import { defineNuxtConfig } from 'nuxt'
-import eslintPlugin from 'vite-plugin-eslint'
-
+import UnpluginComponentsVite from 'unplugin-vue-components/vite'
+import IconsResolver from 'unplugin-icons/resolver'
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  // app
+  app: {
+    head: {
+      title: 'Nuxt 3  Starter',
+      titleTemplate: '%s - Nuxt 3 Awesome Starter',
+      meta: [
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'Nuxt 3  Starter',
+        },
+      ],
+      link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    },
+  },
+  // css
+  css: [
+    'virtual:windi-base.css',
+    'virtual:windi-components.css',
+    'virtual:windi-utilities.css',
+    '~/assets/sass/vendor.scss',
+    '~/assets/sass/app.scss',
+  ],
+  // build
+  build: {
+    transpile: ['@headlessui/vue'],
+  },
+  // modules
+  modules: [
+    'nuxt-windicss',
+    '@intlify/nuxt3',
+    '@vueuse/nuxt',
+    '@nuxt/content',
+    '@pinia/nuxt',
+  ],
+  // build modules
+  buildModules: ['@nuxtjs/eslint-module', 'unplugin-icons/nuxt', '@nuxtjs/svg'],
+  // experimental features
+  experimental: {
+    reactivityTransform: true,
+  },
+  // auto import components
+  components: true,
   vite: {
-    plugins: [eslintPlugin()],
+    plugins: [
+      UnpluginComponentsVite({
+        dts: true,
+        resolvers: [
+          IconsResolver({
+            prefix: 'Icon',
+          }),
+        ],
+      }),
+    ],
+  },
+  // vueuse
+  vueuse: {
+    ssrHandlers: true,
+  },
+  // windicss
+  windicss: {
+    analyze: {
+      analysis: {
+        interpretUtilities: false,
+      },
+      server: {
+        port: 4000,
+        open: false,
+      },
+    },
+    scan: true,
   },
 })
---
+++ pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="text-blue-500">
+    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nesciunt
+    repudiandae eum iste perspiciatis tempore tenetur illo facere totam, quis,
+    ducimus qui molestiae, doloremque animi velit. Vero doloribus tempora ullam
+    architecto.
+  </div>
+</template>
